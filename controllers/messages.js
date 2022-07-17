8f2114fc232facda5637f9e7dc77612140a87f65
Geisson19 <gs.ponce20@uniandes.edu.co>
2022-07-17 16:09:44 -0500

    messages from - controller & logic

--- controllers/messages.js
+++ controllers/messages.js
@@ -2,11 +2,20 @@ const Message = require('../models/message');
 
 const getMessagesFrom = (req, res) => {
 	const myId = req.uid;
-	const messageFrom = req.params.from;
+	const from = req.params.from;
+
+	const last30messages = Message.find({
+		$or: [
+			{ from: myId, to: from },
+			{ from: from, to: myId },
+		],
+	})
+		.sort({ createdAt: 1 })
+		.limit(30);
 
 	res.json({
 		ok: true,
-		message: 'hola',
+		messages: last30messages,
 	});
 };
 
